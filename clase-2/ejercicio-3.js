26ee61919d74ba2d8fd6e38dcec1977fcbbb1aa2
Diaz Martin <elmartind17@gmail.com>
2025-07-16 16:37:21 -0300

    ejercicio 3

---
+++ clase-2/ejercicio-3.js
@@ -0,0 +1,13 @@
+let num1 = parseInt(prompt("Ingrese el primer numero"));
+let num2 = parseInt(prompt("Ingrese el segundo numero"));
+
+resultado = num1 - num2;
+
+console.log(
+  "La resta del",
+  num1,
+  "con el numero",
+  num2,
+  "es igual a",
+  resultado
+);
