8b7e599c672aeb30a6f439ed7122c463b3ab74d8
Diaz Martin <elmartind17@gmail.com>
2025-07-16 16:34:50 -0300

    Se termino el ejercicio 2

---
+++ clase-2/ejercicio-2.js
@@ -0,0 +1,6 @@
+let num1 = parseInt(prompt("Ingrese el primer numero"));
+let num2 = parseInt(prompt("Ingrese el segundo numero"));
+
+resultado = num1 + num2;
+
+console.log("La suma de", num1, "con el numero", num2, "es igual a", resultado);
